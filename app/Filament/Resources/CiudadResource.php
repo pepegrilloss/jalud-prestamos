55c3b1e811a5b3bc176650fd5ed24cf75defcdb3
pepegrilloss <jvilcherrez.01@hotmail.com>
2025-11-07 10:45:34 -0500

    Revierto cambio

--- app/Filament/Resources/CiudadResource.php
+++ app/Filament/Resources/CiudadResource.php
@@ -25,7 +25,7 @@ class CiudadResource extends Resource
     protected static ?string $navigationLabel = 'Ciudades';
 
     protected static ?string $modelLabel = 'Ciudad';
-    protected static ?string $pluralModelLabel = 'Ciudades Te Amo Ariana';
+    protected static ?string $pluralModelLabel = 'Ciudades';
 
     public static function form(Form $form): Form
     {
