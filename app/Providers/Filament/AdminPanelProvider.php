f67a75ea707895e55684e09ad244291b66203200
Julio Mario Vilcherrez Purizaca <julio.vilcherrez@medlogpiura.pe>
2026-03-10 10:58:07 -0500

    Actualización

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -47,7 +47,7 @@ class AdminPanelProvider extends PanelProvider
                 PanelsRenderHook::HEAD_START,
                 fn(): string => '<link rel="stylesheet" href="' . asset('css/login-custom.css') . '?v=' . time() . '">'
             )
-            ->favicon(asset('favicon.ico'))
+            ->favicon(asset('favicon-j.svg'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
